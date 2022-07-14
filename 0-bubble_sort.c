cf59323bff5cbb45291a01881a428865ced83e49
Natiman58 <natiman58@gmail.com>
2022-07-14 13:21:38 +0300

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sorts an array in accordance to bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: size of the array
+ * Returns the sorted list.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, temp;
+	int ar_size = size;
+
+	if (array == NULL || ar_size < 2)
+		return;
+	for (i = 0; i < ar_size - 1; i++)
+	{
+		for (j = 0; j < ar_size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{	temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+
+		}
+	}
+}
