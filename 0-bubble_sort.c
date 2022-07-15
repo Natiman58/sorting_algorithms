7d0db3380c72eef9d7cf5ccd41e9ef34abdf3dfe
Natiman58 <natiman58@gmail.com>
2022-07-15 17:22:19 +0300

    O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,23 +6,24 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        int i, j, temp;
-        int ar_size = size;
+	int i, j, temp;
+	int ar_size = size;
 
-        if (array == NULL || ar_size < 2)
-                return;
+	if (array == NULL || ar_size < 2)
+		return;
 
-        for (i = 0; i < ar_size - 1; i++)
-        {
-                for (j = 0; j < ar_size - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {       temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                                print_array(array, size);
-                        }
-                }
-        }
+	for (i = 0; i < ar_size - 1; i++)
+	{
+		for (j = 0; j < ar_size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
 
