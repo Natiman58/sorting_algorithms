dd74f7e7ae2483ad5aa9866923aa576e4e11b7e3
Natiman58 <natiman58@gmail.com>
2022-07-20 20:16:22 +0300

    comments

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,7 +8,7 @@ void selection_sort(int *array, size_t size)
 {
 	int current_min = 0;
 	int true_min = 0;
-	int flag = 0;
+	int flag = 0; /*Initially true min is not set*/
 	size_t i, j;
 
 	for (i = 0; i < size; i++) /* iterate through the list */
@@ -23,11 +23,11 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < current_min) /*if nxt element is < current min*/
 			{
 				current_min = array[j]; /*current_min becomes the smallest*/
-				true_min = j;
-				flag = 1;
+				true_min = j; /*store the index of true min(j) in true_min*/
+				flag = 1; /*true minimum is found*/
 			}
 		}
-		if (flag == 1)
+		if (flag == 1) /*if true minimum is found; swap with current min*/
 		{
 			array[true_min] = array[i];
 			array[i] = current_min;
